cd8671d58fa9f4f870311302b3a766f9c127d394
Wolfleader <james.morgan@iinet.net.au>
2020-08-25 09:34:04 +0800

    Update Fenrir.js

--- src/Fenrir.js
+++ src/Fenrir.js
@@ -1,19 +1,19 @@
 class FenrirApp extends HTMLElement {
     constructor() {
         super();
-        var css = document.createElement("link");
+    }
+    connectedCallback() {
+        let css = document.createElement("link");
         css.href = "https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
         css.rel = "stylesheet"
         this.appendChild(css)
     }
+
 }
 
 class FenrirTitle extends HTMLElement {
     constructor() {
         super();
-
-        var title_font = new FontFace('Junction Regular', 'url(fonts/junction-regular.woff)');
-
 
         if(this.parentElement.tagName.toLowerCase() != "fenrir-app")
             return alert(`${this.tagName.toLowerCase()} must be in <fenrir-app></fenrir-app>`);
